63903e1c3ec88fe5c270e509705d5ff91821c4e5
Xavier Domingo Burjachs <xavierdomingoburjachs@gmail.com>
2016-02-27 14:44:27 +0100

    Using profiling

--- public/index.php
+++ public/index.php
@@ -7,9 +7,18 @@ require __DIR__ . "/../vendor/autoload.php";
 
 use Mpwarfwk\Component\Bootstrap\Bootstrap;
 use Mpwarfwk\Component\Http\Request\Request;
+use Mpwarfwk\Component\Profiler\ProfilingAggregator;
 
 date_default_timezone_set('Europe/Madrid');
 
 $app = new Bootstrap('../app/services.yml');
 $request = Request::createFromGlobals();
 $app->run($request)->send();
+
+
+$profilingServices = $app->getTags('profiling');
+foreach ($profilingServices as $profilingService) {
+    $services[] = $app->getService($profilingService);
+}
+$profiler = new ProfilingAggregator($services);
+echo $profiler->displayInformation();
